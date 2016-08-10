fda27170abb3ab37b5f6f925482313b05a1d0f80
Alex Lockwood <alockwood@google.com>
2016-08-10 13:23:57 -0400

    update sample app

--- app/src/main/java/com/example/alockwood/themedbuttons/BackgroundTints.java
+++ app/src/main/java/com/example/alockwood/themedbuttons/BackgroundTints.java
@@ -15,7 +15,6 @@ import android.util.TypedValue;
  * Utility class for generating background tint {@link ColorStateList}s.
  */
 final class BackgroundTints {
-
   private static final int[] DISABLED_STATE_SET = new int[]{-android.R.attr.state_enabled};
   private static final int[] PRESSED_STATE_SET = new int[]{android.R.attr.state_pressed};
   private static final int[] FOCUSED_STATE_SET = new int[]{android.R.attr.state_focused};
@@ -23,15 +22,21 @@ final class BackgroundTints {
 
   /**
    * Returns a {@link ColorStateList} that can be used as a colored button's background tint.
+   * Note that this code makes use of the {@code android.support.v4.graphics.ColorUtils}
+   * and {@code android.support.v7.content.res.AppCompatResources} utility classes.
    */
   public static ColorStateList forColoredButton(Context context) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
       // On API 21+ we can extract the ColorStateList from XML directly.
+      // We could also construct the ColorStateList programatically similar to below
+      // (as long as we remember not to include the pressed and focused states
+      // on API 21+, since the button's foreground RippleDrawable will animate these
+      // state changes automatically).
       return AppCompatResources.getColorStateList(context, R.color.btn_colored_background_tint);
     }
 
-    // On older platform versions, we need to construct pressed/focused button
-    // states as well and can only be done programatically.
+    // On older platform versions, there's no easy way to generate the pressed/focused
+    // state colors in XML, so we have to create the ColorStateList programatically.
 
     final int[][] states = new int[4][];
     final int[] colors = new int[4];
@@ -56,16 +61,17 @@ final class BackgroundTints {
 
   @ColorInt
   private static int getDisabledButtonBackgroundColor(Context context) {
-    final float disabledAlpha = getDisabledAlpha(context);
-    final int colorButtonNormal = getThemeAttrColor(context, R.attr.colorButtonNormal);
-    return ColorUtils.setAlphaComponent(
-        colorButtonNormal, Math.round(Color.alpha(colorButtonNormal) * disabledAlpha));
-  }
-
-  private static float getDisabledAlpha(Context context) {
+    // Extract the disabled alpha to apply to the button using the context's theme.
+    // (0.26f for light themes and 0.30f for dark themes).
     final TypedValue tv = new TypedValue();
     context.getTheme().resolveAttribute(android.R.attr.disabledAlpha, tv, true);
-    return tv.getFloat();
+    final float disabledAlpha = tv.getFloat();
+
+    // Use the disabled alpha factor and the button's default normal color
+    // to generate the button's disabled background color.
+    final int colorButtonNormal = getThemeAttrColor(context, R.attr.colorButtonNormal);
+    final int originalAlpha = Color.alpha(colorButtonNormal);
+    return ColorUtils.setAlphaComponent(colorButtonNormal, Math.round(originalAlpha * disabledAlpha));
   }
 
   @ColorInt
